ab2f2f6175d8cb62b3bfab43e40906ddbea82a16
MaxPlay066 <leonemassimo306@gmail.com>
2021-12-24 00:15:13 +0100

    Last Change I hope 1.6.2

--- BetterTesla/Config.cs
+++ BetterTesla/Config.cs
@@ -13,26 +13,14 @@ namespace BetterTesla
         [Description("Teams that should not active the teslas. Teams here: https://controlc.com/4ff2b452")]
         public List<Team> NoTriggerTesla { get; set; } = new List<Team>() { Team.MTF };
 
-        [Description("Is BroadCast Enabled for bypassing tesla for MTF? (default = true) [true/false]")]
-        public bool TeslaMTFBC { get; set; } = true;
-
-        [Description("Is BroadCast Enabled for bypassing tesla for CHI? (default = true) [true/false]")]
-        public bool TeslaCHIBC { get; set; } = true;
-
-        [Description("To Make Change To The Broadcast to the MTF")]
-        public BC TeslaMTF { get; set; } = new BC(" <b><color=red> TESLA GATE DISABLED FOR MTF </color></b>");
-
-        [Description("To Make Change To The Broadcast to the CHI")]
-        public BC TeslaCHI { get; set; } = new BC(" <b><color=green> TESLA GATE DISABLED FOR CHI </color></b>");
-
-        [Description("There is an Item that bypass the Teslas? (default = false) [true/false]")]
+        [Description("Is There an Item that bypass the Teslas? (default = false) [true/false]")]
         public bool IfBypassTeslaItem { get; set; } = false;
 
-        [Description("If there is a bypassteslaitem, what is that? (default = coin) Example: Coin. List (Pick the Name):https://pastebin.com/RUT27JBU")]
+        [Description("If there is a bypass tesla item, what is that? (default = coin) Example: Coin. List (Pick the Name):https://pastebin.com/RUT27JBU")]
         public ItemType BypassTeslaItem { get; set; }= ItemType.Coin;
 
-        [Description("Broadcast pickup item")]
-        public Exiled.API.Features.Broadcast PickItemBC { get; set; } = new Exiled.API.Features.Broadcast(" <color=white> THIS ITEM WILL BYPASS TESLA </color>");
+        [Description("Broadcast pickup item (If you don't want a broadcast for that, just erase the the BC content)")]
+        public BC PickItemBC { get; set; } = new BC(" <color=white> THIS ITEM WILL BYPASS TESLA </color>");
 
         [Description("Cost for using Tesla for SCP 079 (default = 50)")]
         public int TeslaCost079 { get; set; } = 50;
@@ -44,9 +32,9 @@ namespace BetterTesla
         public bool IfTesla079Limit { get; set; } = false;
 
         [Description("If Teslas079Limit is true, set the limit for 079 (default = 15)")]
-        public int Tesla079Limit { get; set; }= 15;
+        public int Tesla079Limit { get; set; } = 15;
 
         [Description("If there isn't any SCP left, should be the teslas disabled? (default = false)")]
-        public bool TeslaDisableAtNoScp { get; set; }= false;
+        public bool TeslaDisableAtNoScp { get; set; } = false;
     }
 }
