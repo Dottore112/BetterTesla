eaf67b7986cc394d3e0aa3165bbe96d226fffd0a
MaxPlay066 <leonemassimo306@gmail.com>
2022-01-05 13:19:21 +0100

    ..

--- BetterTesla/Config.cs
+++ BetterTesla/Config.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 
 namespace BetterTesla
 {
-    public class Config : IConfig
+    public class Config : IConfig//b
     {
         [Description("Enable/Disable the Plugin.")]
         public bool IsEnabled { get; set; } = true;
